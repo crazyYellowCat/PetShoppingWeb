79e59c03379f30f47e476b3af5a5b8a971dc5042
crazyYellowCat <mahuicool@163.com>
2018-11-19 21:13:34 +0800

    第二天

--- TheSecondPorgressProject/assets/js/main.js
+++ TheSecondPorgressProject/assets/js/main.js
@@ -7,11 +7,18 @@ var EPetList=$("[class='myEPet']")
 
 var bookMark=$("[class='bookMark']")
 var myBookMark=$("[class='myBookMark']")
-
+console.log(bookMark)
+var mobile=$("[class='mobile']")
+console.log(mobile)
+var QRCode=$("[class='QRCode']")
 hideAndDisplay(cities,placeHide);
 hideAndDisplay(myEPet,EPetList);
 hideAndDisplay(bookMark,myBookMark);
+hideAndDisplay(mobile,QRCode);
+
+
 function hideAndDisplay(parent,son) {
+    console.log(bookMark)
     console.log(parent,son);
    parent.on("mouseenter mouseleave",clickHandler);
 
